c6569707b19f3bf0651d4cdb009884de6524319f
djmagnusa <pratush.bh@gmail.com>
2021-09-09 03:06:26 +0530

    Apply middleware and import some files

--- server/app.js
+++ server/app.js
@@ -6,6 +6,11 @@ const app = express();
 dotenv.config({path:'./config.env'});
 require('./db/conn');
 
+app.use(express.json())
+
+const User = require('./model/userSchema');
+
+app.use(require('./router/auth'));
 
 const PORT = process.env.PORT;
 
@@ -20,9 +25,9 @@ const middleware = (req, res, next) => {
 
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world from the server');
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello world from the server app.js');
+// })
 
 app.get('/about', middleware, (req, res) => {
     res.send('Hello about world from the server')
