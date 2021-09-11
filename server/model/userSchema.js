fb126b0b3739b58c9b222f60a807c20f830f7750
djmagnusa <pratush.bh@gmail.com>
2021-09-11 13:31:02 +0530

    Add pre save middleware to hash password

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -26,6 +27,17 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+userSchema.pre('save', async function(next) {
+    
+    console.log("Hello from inside");
+    
+    if(this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+        this.cpassword = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
 
 const User = mongoose.model('USER', userSchema) //USER wll be converted to User
 
