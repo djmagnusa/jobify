388b12ef89f69bb1c2374511b728f709d07e9f6a
djmagnusa <pratush.bh@gmail.com>
2021-09-12 21:24:17 +0530

    Initial create-react-app

---
+++ client/src/components/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p>WELCOME</p>
+            <h1>Jobify - Job Profiles</h1>
+        
+        </div>
+    )
+}
