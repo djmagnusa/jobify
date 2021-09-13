26506ce4bf7bd3f77d74b6f45a5d499cc3f5fe32
djmagnusa <pratush.bh@gmail.com>
2021-09-14 03:00:45 +0530

    Style login and signup page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,82 +1,88 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import signpic from "../images/signup.jpg";
+
 
 const Signup = () => {
     return (
         <>
-            <section className="signup">
-                <div className="container mt-5">
-                    <div className="signup-content">
-                        <div className="signup-form">
-                            <h2 className="form-title">Sign up</h2>
-                            <form className="register-form" id="register-form">
-                                <div className="form-group">
-                                    <label htmlFor="name">
-                                        <i className="zmdi zmdi-account material-icons-name"></i>                                        
-                                    </label>
-                                    <input type="text" name="name" id="name" autoComplete="off"
-                                        placeholder="Your Name"
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="email">
-                                        <i className="zmdi zmdi-email material-icons-name"></i>                                        
-                                    </label>
-                                    <input type="email" name="name" id="name" autoComplete="off"
-                                        placeholder="Your Email"
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="work">
-                                        <i className="zmdi zmdi-slideshow material-icons-name"></i>                                        
-                                    </label>
-                                    <input type="text" name="work" id="work" autoComplete="off"
-                                        placeholder="Your Job"
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="password">
-                                        <i className="zmdi zmdi-lock material-icons-name"></i>                                        
-                                    </label>
-                                    <input type="password" name="password" id="password" autoComplete="off"
-                                        placeholder="Your Password"
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="cpassword">
-                                        <i className="zmdi zmdi-lock material-icons-name"></i>                                        
-                                    </label>
-                                    <input type="cpassword" name="cpassword" id="cpassword" autoComplete="off"
-                                        placeholder="Confirm Password"
-                                    />
-                                </div>
-
-                                <div className="form-group form-button">
-                                    <input type="submit" name="signup" id="signup" className="form-submit"
-                                        value="Register"
-                                    />
-                                </div>
-
-                            </form>
-
-                            </div>
-
-                            <div className="signup-image">
-                                <figure>
-                                    <img src={signpic} alt="registration pic" />
-                                </figure>
-                                <NavLink to="/login" className="singup-image-link">I am already registered</NavLink>
-                            </div>
-                            
-                    </div>
-                
+        <div class="signup-form">
+        <form>
+        <h2>Sign Up</h2>
+        <p>Please fill in this form to create an account!</p>
+        <hr />
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <span class="fa fa-user"></span>
+                    </span>
                 </div>
-            </section>
+                <input type="text" class="form-control" name="username" placeholder="Username" required="required" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <i class="fa fa-paper-plane"></i>
+                    </span>
+                </div>
+                <input type="email" class="form-control" name="email" placeholder="Email Address" required="required" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                    <i class="fa fa-phone"></i>
+                    </span>
+                </div>
+                <input type="number" class="form-control" name="mobile" placeholder="Phone Number" required="required" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                    <i class="fa fa-briefcase"></i>
+                    </span>
+                </div>
+                <input type="text" class="form-control" name="work" placeholder="Your Profession" required="required" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <i class="fa fa-lock"></i>
+                    </span>
+                </div>
+                <input type="text" class="form-control" name="password" placeholder="Password" required="required" />
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <i class="fa fa-lock"></i>
+                        <i class="fa fa-check"></i>
+                    </span>
+                </div>
+                <input type="text" class="form-control" name="cpassword" placeholder="Confirm Password" required="required" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="form-check-label"><input type="checkbox" required="required" /> I accept the <NavLink to="/login">Terms of Use</NavLink> &amp; <NavLink to="/login">Privacy Policy</NavLink></label>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
+        </div>
+
+        <div class="text-center">Already have an account? <NavLink to="/login">Login here</NavLink></div>
+
+        </form>
+
+    </div>
         </>
     )
 }
