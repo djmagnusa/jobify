5d1411bade774b00cb7d531e86527039d418f138
djmagnusa <pratush.bh@gmail.com>
2021-09-16 02:44:41 +0530

    Some fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
