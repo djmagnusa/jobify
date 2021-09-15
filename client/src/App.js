829a605df87ef3411e5c277e096f07f25de8f7fb
djmagnusa <pratush.bh@gmail.com>
2021-09-16 02:41:21 +0530

    Some fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -11,33 +12,36 @@ import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <Route exact path="/">
-           <Home />
-      </Route>
 
-      <Route path="/about">
-          <About />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
 
-      <Route path="/contact">
-          <Contact />
-      </Route>
+        <Route path="/about">
+            <About />
+        </Route>
 
-      <Route path="/login">
-          <Login />
-      </Route>
+        <Route path="/contact">
+            <Contact />
+        </Route>
 
-      <Route path="/signup">
-          <Signup />
-      </Route>
+        <Route path="/login">
+            <Login />
+        </Route>
 
-      <Route>
-        <NotFound />
-      </Route>
+        <Route path="/signup">
+            <Signup />
+        </Route>
+
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
       
-    </div>
+    </>
   )
 }
 
