206739772df558022d539b5371c2f7a1d6c0d332
djmagnusa <pratush.bh@gmail.com>
2021-09-18 02:25:28 +0530

    Add /contact route

--- server/router/auth.js
+++ server/router/auth.js
@@ -129,6 +129,34 @@ router.get('/getdata', authenticate, (req, res) => {
 });
 
 
+//contact us page
+
+router.post('/contact', authenticate , async (req, res) => {
+    try {
+        const {name, email, phone, message} = req.body;
+
+        if(!name || !email || !phone || !message){
+            console.log("error in contact form");
+            return res.json({ error: "Fill the contact form"})
+        }
+
+        const userContact = await User.findOne({_id: req.userID})
+
+        if(userContact) {
+            const userMessage = await userContact.addMessage(name, email, phone, message);
+            
+            await userContact.save();
+
+            res.status(201).json({ message: "user contact successfully"});
+        }
+
+
+    } catch(error) {
+        console.log(error);
+    }
+})
+
+
 
 
 module.exports = router;
